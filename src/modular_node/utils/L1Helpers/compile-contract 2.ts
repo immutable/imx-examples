f1e4bd27606573ea3f604c4d1819f069b5f8e010
samueltan-immutable <91454198+samueltan-immutable@users.noreply.github.com>
2022-05-06 06:50:03 +1000

    Remove duplicates

--- src/modular_node/utils/L1Helpers/compile-contract 2.ts
+++
@@ -1,6 +0,0 @@
-import hre from 'hardhat';
-
-//Compiles the all the contracts in src/L1/contracts, contracts path is defined in hardhat.config.ts
-export async function compileContract() {
-    await hre.run('compile');
-}
--- src/modular_node/utils/postHelpers/add-metadata-schema 2.ts
+++
@@ -1,7 +0,0 @@
-import { AddMetadataSchemaToCollectionResult, AddMetadataSchemaToCollectionParams, MetadataTypes } from '@imtbl/imx-sdk';
-import { getClient } from '../client';
-
-export async function addMetadataSchemaToCollection(ownerPrivateKey: string, tokenAddress: string, schema: AddMetadataSchemaToCollectionParams, network: string): Promise<AddMetadataSchemaToCollectionResult> {
-    const client = await getClient(network, ownerPrivateKey);
-    return client.addMetadataSchemaToCollection(tokenAddress, schema);
-}
