7f4927493dc20d506c1881430f59756856481b17
Nik Ho <studying.mole@gmail.com>
2024-03-08 15:27:51 +1300

    add create project script

---
+++ src/onboarding/2-create-project.ts
@@ -0,0 +1,5 @@
+/**
+ * This is the second step of the onboarding process.
+ * Follow the guide at https://docs.immutable.com/docs/x/launch-collection/register-project
+ * to create a project in the Immutable Hub(https://hub.immutable.com).
+ */
