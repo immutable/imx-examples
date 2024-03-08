7c5032a806e5364a77fef084941f66bcbc6e9afb
Nik Ho <studying.mole@gmail.com>
2024-03-08 15:31:27 +1300

    update env examples

--- src/config/client.ts
+++ src/config/client.ts
@@ -1,20 +1,9 @@
 import { getEnv } from '../libs/utils';
 
 export default {
+  // general
   alchemyApiKey: getEnv('ALCHEMY_API_KEY'),
   ethNetwork: getEnv('ETH_NETWORK'),
-  client: {
-    publicApiUrl: getEnv('PUBLIC_API_URL'),
-    starkContractAddress: getEnv('STARK_CONTRACT_ADDRESS'),
-    registrationContractAddress: getEnv('REGISTRATION_ADDRESS'),
-    gasLimit: getEnv('GAS_LIMIT'),
-    gasPrice: getEnv('GAS_PRICE'),
-  },
-  // Bulk minting
-  privateKey1: getEnv('PRIVATE_KEY1'),
-  tokenId: getEnv('TOKEN_ID'),
-  tokenAddress: getEnv('TOKEN_ADDRESS'),
-  bulkMintMax: getEnv('BULK_MINT_MAX'),
   // Onboarding
   ownerAccountPrivateKey: getEnv('OWNER_ACCOUNT_PRIVATE_KEY'),
   collectionContractAddress: getEnv('COLLECTION_CONTRACT_ADDRESS'),
